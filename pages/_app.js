2bf1a4c798278a99b958f26ee899e8ccbcd7e946
MRida2 <136327814+MRida2@users.noreply.github.com>
2024-08-08 16:42:42 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,10 @@
-import "../styles/index.css";
-import { Fragment } from "react";
-import { DefaultSeo } from "next-seo";
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Fragment>
-      <DefaultSeo
-        title="Youtube Thumbnail Downloader"
-        description="Download high-quality thumbnails from YouTube videos."
-        canonical="https://your-website-url.com"
-        openGraph={{
-          url: "https://your-website-url.com",
-          title: "Youtube Thumbnail Downloader",
-          description: "Download high-quality thumbnails from YouTube videos.",
-          site_name: "Youtube Thumbnail Downloader",
-        }}
-      />
+    <React.Fragment>
       <Component {...pageProps} />
-    </Fragment>
+    </React.Fragment>
   );
 }
 
